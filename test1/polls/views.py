571e09a8864f8a7d59452e7ac7753513d0f52343
郭思义 <gsy@meitu.com>
2019-02-09 19:52:09 +0800

    [optimize] 添加模板语法，添加polls的url的空

--- test1/polls/views.py
+++ test1/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.template import loader
@@ -9,18 +9,24 @@ from polls.models import Question
 
 def index(request):
     last_question_list = Question.objects.order_by('pub_date');
-
-    template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list':last_question_list
+        'latest_question_list': last_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
-    return HttpResponse("you are looking at the detail of question %s"%question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
-    return HttpResponse("you are looking at the results of question %s"%question_id)
+    return HttpResponse("you are looking at the results of question %s" % question_id)
+
 
 def vote(request, question_id):
-    return HttpResponse("you are voting at the of question %s"%question_id)
+    return HttpResponse("you are voting at the of question %s" % question_id)
