db2d4b7c937bdaf0b68fb3445fc1e8f91cdd93c9
郭思义 <gsy@meitu.com>
2019-02-09 17:42:13 +0800

    [optimize] 添加几个url路径

--- test1/polls/views.py
+++ test1/polls/views.py
@@ -2,5 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from polls.models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index")
+    last_question_list = Question.objects.order_by('pub_date');
+    output = ', '.join([q.question_text for q in last_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at the detail of question %s"%question_id)
+
+def results(request, question_id):
+    return HttpResponse("you are looking at the results of question %s"%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you are voting at the of question %s"%question_id)
