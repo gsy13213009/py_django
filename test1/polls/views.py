012a53c3c05dc26d71f918d2312649c466321baa
郭思义 <gsy@meitu.com>
2019-02-09 19:25:47 +0800

    [optimize] 添加模板

--- test1/polls/views.py
+++ test1/polls/views.py
@@ -2,13 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
 from polls.models import Question
 
 
 def index(request):
     last_question_list = Question.objects.order_by('pub_date');
-    output = ', '.join([q.question_text for q in last_question_list])
-    return HttpResponse(output)
+
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':last_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("you are looking at the detail of question %s"%question_id)
