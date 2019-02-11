0301d766886b6afa3c2ced213b3faab0773b5327
郭思义 <gsy@meitu.com>
2019-02-11 18:10:28 +0800

    [optimize] 添加注册逻辑的实现

--- dailyfresh/df_user/urls.py
+++ dailyfresh/df_user/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('register/', views.register)
+    url('register/', views.register),
+    url('register_handle/', views.register_handle),
 ]
--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from hashlib import sha1
+from django.http import *
 
 # Create your views here.
+from df_user.models import UserInfo
+
+
 def register(request):
     return render(request, 'df_user/register.html')
+
+def register_handle(request):
+    post = request.POST
+    uname = post.get('user_name')
+    upwd = post.get('pwd')
+    upwd2 = post.get('cpwd')
+    uemail = post.get('email')
+
+    # 判断两次密码是否一致
+    if upwd != upwd2:
+        return redirect('/user/register')
+    # 加密
+    s1 = sha1()
+    s1.update(upwd.encode('utf-8'))
+    upwd3 = s1.hexdigest()
+    # 创建对象
+    user = UserInfo()
+    user.uname = uname
+    user.upwd = upwd3
+    user.uemail = uemail
+    user.save()
+
+    return redirect('/user/login')
+    # return HttpResponse('ok')
