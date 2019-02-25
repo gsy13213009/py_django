64bcf7b84476dbbe03749cf7b55ea88fcc8b5bae
郭思义 <gsy@meitu.com>
2019-02-25 21:22:16 +0800

    [optimiz] 新建项目地址，返回简单内容

--- download/download/settings.py
+++ download/download/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'download.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'download_info',
+        'USER': 'gsy',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
