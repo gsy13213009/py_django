7b64c91d1f4e13992b96575889a1520ba1425d2f
郭思义 <gsy@meitu.com>
2019-02-28 14:52:29 +0800

    [optimize] 增加log日志的配置和使用

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,13 @@
+import logging
 from django.shortcuts import render
 
 # Create your views here.
 
+logger = logging.getLogger('blog.views')
+
 def index(request):
+    try:
+        open('sss.txt', 'r')
+    except Exception as e:
+        logger.error(e)
     return render(request, 'index.html', locals())
