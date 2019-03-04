25d30ce286e93b15d7d75e397a2c284f308e02c9
郭思义 <gsy@meitu.com>
2019-03-04 15:59:25 +0800

    [optimize] 注册所有的mode到admin中

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import *
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('title', 'desc', 'content')
+
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
