234bbd2f8a11553e89988b89053e0fea19a47118
郭思义 <gsy@meitu.com>
2019-03-04 22:42:15 +0800

    [optimize] 初步实现从数据库中读取分类传递给模板

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,18 +2,20 @@ import logging
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Category
+
 logger = logging.getLogger('blog.views')
 from django.conf import settings
 
+
 def global_setting(requst):
     return {
-        'SITE_NAME':settings.SITE_NAME,
-        'SITE_DESC':settings.SITE_DESC,
+        'SITE_NAME': settings.SITE_NAME,
+        'SITE_DESC': settings.SITE_DESC,
     }
 
+
 def index(request):
-    try:
-        open('sss.txt', 'r')
-    except Exception as e:
-        logger.error(e)
-    return render(request, 'index.html', locals())
+    category_list = Category.objects.all()
+
+    return render(request, 'index.html', {'category_list': category_list})
