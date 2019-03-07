bcc7cc6ccf93678abc57827df9be140d18ab1a20
郭思义 <gsy@meitu.com>
2019-03-07 11:49:12 +0800

    [optimize] views的重构，将通用的数据放到global_setting里面

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -12,14 +12,21 @@ from django.conf import settings
 
 
 def global_setting(requst):
+    category_list = Category.objects.all()
+    archive_list = Article.objects.distinct_date()
+    # 标签云
+    # 广告
+    # 友情链接
+
     return {
+        'category_list': category_list,
+        'archive_list': archive_list,
         'SITE_NAME': settings.SITE_NAME,
         'SITE_DESC': settings.SITE_DESC,
     }
 
 
 def index(request):
-    category_list = Category.objects.all()
     # 获取所有的文章数量
     article_list = Article.objects.all()
     # 初始化分页器Paginator
@@ -29,27 +36,6 @@ def index(request):
         article_list = paginator.page(page)
     except:
         article_list = paginator.page(1)
-    try:
-        # 文章归档，先获取文章中的 年-月
-        # print(Article.objects.values('date_publish').distinct())
-
-        #  第一种执行sql的方式
-
-        # 因为一个%代表占位，所以使用%%，第一个把第二个转义一下
-        # archive_list = Article.objects.raw('SELECT DISTINCT id, date_publish, DATE_FORMAT(date_publish, "%%Y-%%m") as col_date FROM blog_article ORDER BY date_publish')
-        # for archive in archive_list:
-        #     print(archive)
-
-        # 第二种方式，使用connection去执行数据库代码
-
-        # cursor = connection.cursor()
-        # cursor.execute('SELECT DISTINCT id, date_publish, DATE_FORMAT(date_publish, "%%Y-%%m") as col_date FROM blog_article ORDER BY date_publish')
-        # row = cursor.fetchall()
-        # print(row)
-        archive_list = Article.objects.distinct_date()
-
-    except Exception as e:
-        print(e)
     return render(request, 'index.html', locals())
 
 
@@ -58,7 +44,7 @@ def archive(request):
     month = request.GET.get('month', None)
 
     # 获取文章数量
-    article_list = Article.objects.filter(date_publish__icontains=year+'-'+month)
+    article_list = Article.objects.filter(date_publish__icontains=year + '-' + month)
     # 初始化分页器Paginator
     paginator = Paginator(article_list, 2)
     try:
